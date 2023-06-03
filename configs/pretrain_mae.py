c26394873b5a87e4f404bd5a2975cb6651c1b42a
Akash Chaurasia <akashchaurasia17@yahoo.com>
2023-06-03 15:37:24 +0000

    pretrain configs inherit from other

--- configs/pretrain_mae.py
+++ configs/pretrain_mae.py
@@ -1,54 +1,11 @@
-import ml_collections
+from configs import pretrain_all
 
 
 def get_config():
-    config = ml_collections.ConfigDict()
-
-    # random seeds
-    config.seed = 42
+    config = pretrain_all.get_config()
 
     # pretraining objectives
-    config.mae = True
     config.denoise = False
     config.causal = False
-
-    # optimizer
-    config.learning_rate = 3e-4
-    config.lr_warmup_steps = 500
-    config.lr_cosine_decay = True
-    config.beta1 = 0.9
-    config.beta2 = 0.95
-    config.weight_decay = 0.05
-    config.batch_size = 1024
-    config.epochs = 5
-
-    # model
-    config.width = 768
-    config.layers = 12
-    config.heads = 12
-    config.image_size = 224
-    config.patch_size = 16
-
-    config.noise_std = 0.8
-    config.mask_ratio = 0.75
-    config.decoder_layers = 8
-    config.decoder_width = 512
-    config.decoder_heads = 16
-
-    config.attn_dropout_rate = 0
-    config.dropout_rate = 0
-
-    config.train_data = 'imagenet-1k'
-    config.train_num_samples = 1281167
-    config.image_key = 'image'
-
-    # dataloader
-    config.num_workers = 8
-
-    # logging
-    config.wandb = True
-    config.logging_interval = 10
-    config.eval_interval = 500
-    config.ckpt_interval_epochs = 1
 
     return config
